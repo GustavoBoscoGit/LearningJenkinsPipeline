d5d06751bddf592692fc433ce95d8c8fc82b99db
GustavoBoscoGit <gustavobosco1@gmail.com>
2022-06-08 19:41:13 -0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
   screenshotOnRunFailure: true,
@@ -17,5 +18,6 @@ module.exports = defineConfig({
     embeddedScreenshots: true,
     inlineAssets: true,
   },
-  screenshotsFolder: "cypress/screenshot/test2.cy.js",
+  screenshotsFolder: "cypress/screenshot/",
 });
+
