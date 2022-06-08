0d95ad697eed0255ee1b6613ef4d0b97dce91801
GustavoBoscoGit <gustavobosco1@gmail.com>
2022-06-08 19:55:21 -0300

    kjads

--- cypress.config.js
+++ cypress.config.js
@@ -6,18 +6,12 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-  screenshotOnRunFailure: true,
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     reportTitle: "Cypress Test Report",
     reportDir: "cypress/report",
     overwrite: true,
     html: true,
-    charts: true,
     json: true,
-    embeddedScreenshots: true,
-    inlineAssets: true,
   },
-  video: false,
-  screenshotsFolder: "images",
 });
