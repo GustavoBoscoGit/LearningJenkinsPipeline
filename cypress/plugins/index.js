acd251c86e0afaa4957e3ed47d11d26136afb00b
GustavoBoscoGit <gustavobosco1@gmail.com>
2022-06-08 15:07:19 -0300

    test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -21,16 +21,20 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 }
 
-const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
-
-module.exports = (on) => {
-  on('before:run', async (details) => {
-    console.log('override before:run');
-    await beforeRunHook(details);
-  });
-
-  on('after:run', async () => {
-    console.log('override after:run');
-    await afterRunHook();
-  });
+module.exports = (on, config) => {
+  require('cypress-mochawesome-reporter/plugin')(on);
 };
+
+// const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+
+// module.exports = (on) => {
+//   on('before:run', async (details) => {
+//     console.log('override before:run');
+//     await beforeRunHook(details);
+//   });
+
+//   on('after:run', async () => {
+//     console.log('override after:run');
+//     await afterRunHook();
+//   });
+// };
