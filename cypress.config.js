150f91138bda8a790b36d6559a039bb2658f5a3e
GustavoBoscoGit <gustavobosco1@gmail.com>
2022-06-08 20:02:07 -0300

    isss

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-  reporter: "cypress-mochawesome-reporter",
+  reporter: "mochawesome",
   reporterOptions: {
     reportTitle: "Cypress Test Report",
     reportDir: "cypress/report",
