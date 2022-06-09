6c07700a2173ffefc7f4b724bf46c89b1052c708
GustavoBoscoGit <gustavobosco1@gmail.com>
2022-06-09 16:47:24 -0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,41 +1,42 @@
 const { defineConfig } = require("cypress");
 
-const fs = require('fs')
+const fs = require("fs");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('after:screenshot', (details) => {
-        const fileName = details.takenAt.replace(/:/g,".") +".png"
+      on("after:screenshot", (details) => {
+        const fileName = details.takenAt.replace(/:/g, ".") + ".png";
 
         //const fileName = details.specName +".1png"; // This fail
         //const fileName = "screenshot.png"; // This works
-        const newPath = "cypress/mochareports/screenshots/"+ fileName;
+        const newPath = "cypress/mochareports/screenshots/" + fileName;
 
         return new Promise((resolve, reject) => {
-            // fs.rename moves the file to the existing directory 'new/path/to'
-            // and renames the image to 'screenshot.png'
-            fs.rename(details.path, newPath, (err) => {
-                if (err) return reject(err)
+          // fs.rename moves the file to the existing directory 'new/path/to'
+          // and renames the image to 'screenshot.png'
+          fs.rename(details.path, newPath, (err) => {
+            if (err) return reject(err);
 
-                // because we renamed and moved the image, resolve with the new path
-                // so it is accurate in the test results
-                resolve({ path: newPath })
-            })
-        })
-    })
-    }
+            // because we renamed and moved the image, resolve with the new path
+            // so it is accurate in the test results
+            resolve({ path: newPath });
+          });
+        });
+      });
+    },
   },
   screenshotOnRunFailure: true,
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     reportTitle: "Cypress Test Report",
+    charts: true,
     reportDir: "cypress/report",
     overwrite: true,
-    html: true,
-    json: false,
+    html: false,
+    json: true,
     embeddedScreenshots: true,
-    inlineAssets: true,
+    screenshotOnRunFailure: true,
   },
   video: false,
   screenshotsFolder: "cypress/mochareports/screenshots/",
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -24,29 +24,3 @@ module.exports = (on, config) => {
 module.exports = (on, config) => {
   require('cypress-mochawesome-reporter/plugin')(on);
 };
-
-//Screenshots names can be too long for the file system, taht why we truncate them
-module.exports = (on, config) => {
-  on('after:screenshot', (details) => {
-      console.log(details) // print all details to terminal
-      //const fileName = details.takenAt.replace(":",".") +".png"; // This fails
-
-      const fileName = details.takenAt.replace(" ","-","(",")") +".png"; // This fails
-      console.log(fileName);
-      //const fileName = "test.png"; // This works
-      const newPath = "cypress/screenshots/"+ fileName;
-      console.log(newPath);
-
-      return new Promise((resolve, reject) => {
-          // fs.rename moves the file to the existing directory 'new/path/to'
-          // and renames the image to 'screenshot.png'
-          fs.rename(details.path, newPath, (err) => {
-              if (err) return reject(err)
-
-              // because we renamed and moved the image, resolve with the new path
-              // so it is accurate in the test results
-              resolve({ path: newPath })
-          })
-      })
-  })
-}
